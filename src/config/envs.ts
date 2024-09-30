f291bc3b3816aa4637a0b1ac2440c83ed371d752
Juan pablo acer <juanpablo310446@gmail.com>
2024-09-29 22:40:39 -0500

    first commit Stripe implementation

---
+++ src/config/envs.ts
@@ -0,0 +1,23 @@
+import 'dotenv/config';
+import * as joi from 'joi';
+import { IEnvVarsInterface } from 'src/interfaces/env-vars.interface';
+
+const envsSchema = joi
+  .object({
+    PORT: joi.number().required(),
+    STRIPE_SECRET: joi.string().required(),
+  })
+  .unknown(true);
+
+const { error, value } = envsSchema.validate(process.env);
+
+if (error) {
+  throw new Error(`Config validation error: ${error.message}`);
+}
+
+const envsVars: IEnvVarsInterface = value;
+
+export const envs = {
+  port: envsVars.PORT,
+  stripe_secret: envsVars.STRIPE_SECRET,
+};
---
+++ src/interfaces/env-vars.interface.ts
@@ -0,0 +1,6 @@
+export interface IEnvVarsInterface {
+  PORT: number;
+  DATABASE_URL: string;
+
+  STRIPE_SECRET: string;
+}
